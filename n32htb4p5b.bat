7c05aaebc31f650b96d8ddaa6161ed10cc03c14f
Rashi Kejriwal <72417676+rashikejriwa@users.noreply.github.com>
2020-12-18 19:00:20 -0500

    Delete n32htb4p5b.bat

--- n32htb4p5b.bat
+++
@@ -1,3 +0,0 @@
-"C:\Users\rashi\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\rashi\OneDrive\Desktop\Lithophane.py\lithophane.py\userInterface.py"
-@echo off
-start /b  cmd /c del /A "C:\Users\rashi\OneDrive\Desktop\Lithophane.py\lithophane.py\n32htb4p5b.bat"&exit /b
